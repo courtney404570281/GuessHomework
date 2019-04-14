80287690ad3e0599825de19058628484fadd5233
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-14 15:44:59 +0800

    Homework1

--- app/src/main/java/com/courtney/guesshomework/MainActivity.kt
+++ app/src/main/java/com/courtney/guesshomework/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.courtney.guesshomework
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val secretNumber = SecretNumber()
+    val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,24 +19,45 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            AlertDialog.Builder(this)
+                .setMessage(getString(R.string.Message))
+                .setMessage(getString(R.string.restart))
+                .setPositiveButton(getString(R.string.ok)) { dialog, which ->
+                    secretNumber.restart()
+                    counter.setText(secretNumber.count.toString())
+                    edt_number.setText("")
+                    Log.d(TAG, "secret: ${secretNumber.secret}")
+                }
+                .setNeutralButton(getString(R.string.cancel), null)
+                .show()
         }
+
+        counter.setText(secretNumber.count.toString())
+
+        Log.d(TAG, "secret: ${secretNumber.secret}")
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    fun check(view : View) {
+        val n = edt_number.text.toString().toInt()
+        println("n: $n")
+        Log.d(TAG, "number: $n")
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        val diff = secretNumber.validate(n)
+
+        var message = when {
+            diff < 0 -> getString(R.string.Higher)
+            diff > 0 -> getString(R.string.Lower)
+            diff == 0 && secretNumber.count < 3 -> "${getString(R.string.excellent)} ${secretNumber.secret}"
+            else ->  getString(R.string.bingo)
         }
+
+        counter.setText(secretNumber.count.toString())
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.Message))
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok), null)
+            .show()
+
     }
 }
